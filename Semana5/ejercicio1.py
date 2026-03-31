33970d03a40c8439b48bdbdd7d8be9e4d94c2bdc
estelita286 <estelafrangarrido@gmail.com>
2026-03-31 15:09:48 -0300

    holis

--- Semana5/ejercicio1.py
+++ Semana5/ejercicio1.py
@@ -10,8 +10,8 @@ def contar_ciclo(n):
     """
     Retorna una lista con los números desde 1 hasta n usando iteración.
     """
-    # Escriba aquí su solución y borre la palabra pass de acontinuación
-    pass
+    for i in range(1,n):
+        return range 
 
 
 def contar_recursivo(n):
